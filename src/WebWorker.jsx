98d9a5bf177c9e15ae1abbcdd981d493a7671358
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2023-03-09 21:07:59 +0200

    feat(web-worker-refactor): refactor and use functional and hooks over class component

--- src/WebWorker.jsx
+++ src/WebWorker.jsx
@@ -1,57 +1,61 @@
-import React, { Component } from "react";
+import React, { useState, useRef, useEffect } from "react";
+
 import workerScript from "./worker";
 
-class WebWorker extends Component {
-  constructor() {
-    super();
-    this.state = {
-      num: "",
-      result: "",
-      loadingMessage: "",
-    };
-    this.handleCount = this.handleCount.bind(this);
-    this.handleCalculate = this.handleCalculate.bind(this);
-  }
+const initialState = { num: 0, result: null, loadingMessage: "" };
 
-  componentDidMount() {
-    this.worker = new Worker(workerScript);
-    this.worker.addEventListener("message", (e) => {
+const WebWorker = () => {
+  const [state, setState] = useState(initialState);
+  const worker = useRef(null);
+
+  const { num, loadingMessage, result } = state;
+
+  const onCountChangeHandler = (e) => {
+    const num = e.target.value;
+    setState({ num });
+  };
+
+  const onCalculateHandler = () => {
+    worker.current.postMessage(num);
+  };
+
+  useEffect(() => {
+    const onWorkerHandler = (e) => {
       const type = e.data.type;
       if (type === "loading") {
         const { i, num } = e.data;
-        this.setState({
-          loadingMessage: `Loaded ${i} / ${num}`,
-        });
+        setState((prevState) => ({
+          ...prevState,
+          num,
+          loadingMessage: `Loading ${i} / ${num}`,
+        }));
       } else {
         const { result } = e.data;
-        this.setState({
+
+        setState((prevState) => ({
+          ...prevState,
           result,
-        });
+          loadingMessage: `Finished Loading => result: (${result})`,
+        }));
       }
-    });
-  }
-  handleCount(e) {
-    this.setState({
-      num: e.target.value,
-    });
-  }
-  handleCalculate() {
-    const { num } = this.state;
-    this.worker.postMessage(num);
-  }
+    };
 
-  render() {
-    const { num, loadingMessage, result } = this.state;
+    worker.current = new Worker(workerScript);
+    worker.current.addEventListener("message", onWorkerHandler);
 
-    return (
-      <div>
-        <input type="number" value={num} onChange={this.handleCount} />
-        <button onClick={this.handleCalculate}>Calculate</button>
-        <h1>{loadingMessage}</h1>
-        <h2>{result}</h2>
-      </div>
-    );
-  }
-}
+    return () => {
+      worker.current.removeEventListener("message", onWorkerHandler);
+    };
+  }, []);
+
+  return (
+    <div>
+      <input type="number" value={num} onChange={onCountChangeHandler} />
+      <button onClick={onCalculateHandler}>Calculate</button>
+      {!!loadingMessage && <h1>{loadingMessage}</h1>}
+      {result && <h2>{result}</h2>}
+    </div>
+  );
+};
 
 export default WebWorker;
