4e3f34c6365acc5e4a67c2763cfe430b88daafaf
Vinicius Cerqueira Bonifácio <vinicius.bonifacio1985@gmail.com>
2023-04-26 21:59:44 +0300

    feat(utils): helper for hardcoded image info and route path normalizer

---
+++ src/utils/images.ts
@@ -0,0 +1,31 @@
+export interface INavItemInfo {
+  link: string
+  pageName: string
+}
+
+export const navItems: INavItemInfo[] = [
+  {
+    link: 'https://images.unsplash.com/photo-1666091863721-54331a5db52d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    pageName: 'Home',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1666055642230-1595470b98fd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=995&q=80',
+    pageName: 'Work',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1666005487638-61f45819c975?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    pageName: 'About',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1665910407771-bc84ad45676b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80',
+    pageName: 'Contact',
+  },
+  {
+    link: 'https://images.unsplash.com/photo-1553356084-58ef4a67b2a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',
+    pageName: 'Join Us',
+  },
+]
+
+export const pathNormalizer = (path: string): string => {
+  return path.split(' ').join('').toLowerCase()
+}
